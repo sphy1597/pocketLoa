54e4658426d038e5dde30a32b75b5ece197b05ff
sphy1597 <sphy1597@naver.com>
2024-03-22 17:34:48 +0900

    feat: bottom nav

--- app/src/main/java/com/example/pocketloa/view/AuctionFragment.kt
+++ app/src/main/java/com/example/pocketloa/view/AuctionFragment.kt
@@ -1,60 +1,37 @@
 package com.example.pocketloa.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.lifecycle.ViewModelProvider
 import com.example.pocketloa.R
+import com.example.pocketloa.view.auction.AuctionRVActivity
+import com.example.pocketloa.view.auction.AuctionSelectActivity
+import com.example.pocketloa.view.auction.AuctionViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AuctionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AuctionFragment : Fragment() {
-	// TODO: Rename and change types of parameters
-	private var param1: String? = null
-	private var param2: String? = null
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		arguments?.let {
-			param1 = it.getString(ARG_PARAM1)
-			param2 = it.getString(ARG_PARAM2)
-		}
-	}
+	private lateinit var viewModel : AuctionViewModel
+
 
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View? {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_auction, container, false)
+		val view = inflater.inflate(R.layout.fragment_auction, container, false)
+		viewModel = ViewModelProvider(this).get(AuctionViewModel::class.java)
+
+		view.findViewById<Button>(R.id.auction_fragment_btn).setOnClickListener {
+			val intent = Intent(requireContext(), AuctionSelectActivity::class.java)
+			startActivity(intent)
+		}
+		return view
 	}
 
-	companion object {
-		/**
-		 * Use this factory method to create a new instance of
-		 * this fragment using the provided parameters.
-		 *
-		 * @param param1 Parameter 1.
-		 * @param param2 Parameter 2.
-		 * @return A new instance of fragment AuctionFragment.
-		 */
-		// TODO: Rename and change types and number of parameters
-		@JvmStatic
-		fun newInstance(param1: String, param2: String) =
-			AuctionFragment().apply {
-				arguments = Bundle().apply {
-					putString(ARG_PARAM1, param1)
-					putString(ARG_PARAM2, param2)
-				}
-			}
-	}
+
 }
